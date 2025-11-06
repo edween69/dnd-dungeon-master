699cc76c24c4d74196cf00a10ce80efb141d8aba
Edwin Baiden <ebaiden64@gmail.com>
2025-11-05 19:50:38 -0500

    Declared openStartingStatsCSV and storeAllStatLines functions in characters.h and added characters.cpp to the compile list in the makefile

--- src/characters.h
+++ src/characters.h
@@ -262,4 +262,7 @@ class Atilla : public PlayerCharacter
         };
 };
 
+std::ifstream* openStartingStatsCSV();
+std::istringstream* storeAllStatLines(std::ifstream* statsFile);
+
 #endif // CHARACTERS_H
